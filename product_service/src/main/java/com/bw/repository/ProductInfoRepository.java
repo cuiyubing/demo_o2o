bae8d680ae1faf85230290e595d950efabaf63e3
bingbing <1565570355@qq.com>
2019-11-14 20:36:21 +0800

    项目初始化

---
+++ product_service/src/main/java/com/bw/repository/ProductInfoRepository.java
@@ -0,0 +1,18 @@
+package com.bw.repository;
+
+import com.bw.pojo.ProductCategory;
+import com.bw.pojo.ProductInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProductInfoRepository extends JpaRepository<ProductInfo,String>{
+
+
+//   List<ProductInfo> findAllByCategory_type();
+
+    List<ProductInfo> findAllByProductStatusAndCategoryType(Integer status, Integer type);
+
+
+
+}
